85bded56474639108c994065c410b50bc48b09da
Kartikeya Bhargava <kartikeyabhargava95@gmail.com>
2017-09-07 13:17:41 +0530

    Search by tx hash feature added

--- routes/search.js
+++ routes/search.js
@@ -3,14 +3,22 @@ var router = express.Router();
 
 router.post('/', function(req, res, next) {
 	var searchString = req.body.search.trim().toLowerCase();
-	
-  if (searchString.length > 22 && searchString.substr(0,2) != '0x')
+	var db = req.app.get('db');
+  if ((searchString.length === 40 || searchString.length === 64) && searchString.substr(0,2) != '0x')
     searchString = '0x' + searchString;
-    
+
 	if (searchString.length === 2) {
 		return next({ message: "Error: Invalid search string!" });
-	} else {
+	}
+	else if (searchString.length === 42) {
 		res.redirect('/account/' + searchString);
+	}
+	else if (searchString.length === 66) {
+		db.find({transactionHash: searchString}).exec(function (err, event) {
+		if (err) {
+			return next(err);
+	  }
+		res.redirect('/event/' + event[0]._id)});
 	}
 });
 

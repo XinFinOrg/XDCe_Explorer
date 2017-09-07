616d0f6824951415e0d0a77ea23dcf3f2e2984b9
Kartikeya Bhargava <kartikeyabhargava95@gmail.com>
2017-09-07 15:34:11 +0530

    Bug fix - Invalid hash search server crash

--- routes/search.js
+++ routes/search.js
@@ -4,9 +4,9 @@ var router = express.Router();
 router.post('/', function(req, res, next) {
 	var searchString = req.body.search.trim().toLowerCase();
 	var db = req.app.get('db');
-  if ((searchString.length === 40 || searchString.length === 64) && searchString.substr(0,2) != '0x')
+  if ((searchString.length === 40 || searchString.length === 64) && searchString.substr(0,2) != '0x') {
     searchString = '0x' + searchString;
-
+	}
 	if (searchString.length === 2) {
 		return next({ message: "Error: Invalid search string!" });
 	}
@@ -15,13 +15,16 @@ router.post('/', function(req, res, next) {
 	}
 	else if (searchString.length === 66) {
 		db.find({transactionHash: searchString}).exec(function (err, event) {
-		if (err) {
-			return next(err);
-	  }
-		res.redirect('/event/' + event[0]._id)});
+			if (err){
+				return next(err);
+			}
+			if (event.length === 0 || !event[0]._id) {
+	      return next({message: "Transaction not found"});
+	    }
+			res.redirect('/event/' + event[0]._id)});
 	}
 	else {
-		return next({ message: "Error: Invalid search string!" });
+		return next({ message: "Error: Please enter a valid address or transaction hash" });
 	}
 });
 
